eea93ae45fcf0f160ae66ea10c3dc38ed2ac5017
Abbin44 <abbin44@gmail.com>
2022-05-22 14:53:09 +0200

    Repaired GOL to actually work

--- Conways Game Of Life/Program.cs
+++ Conways Game Of Life/Program.cs
@@ -18,12 +18,12 @@ namespace Cells
         {
             string numbers = "0123456789";
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Enter width: 20 is recommended");
-            width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter height: 20 is recommended");
+            height = Convert.ToInt32(Console.ReadLine());
             Console.Clear();
 
-            Console.WriteLine("Enter height: 40 is recommended");
-            height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter width: 40 is recommended");
+            width = Convert.ToInt32(Console.ReadLine());
             Console.Clear();
 
             DrawStartArea();
@@ -33,30 +33,32 @@ namespace Cells
 
         static void DrawStartArea()
         {
-            current = new char[width, height];
-            coming = new char[width, height];
+            current = new char[height, width];
+            coming = new char[height, width];
 
             //generate a random number to decide what cells starts as living
             //Random rng = new Random();
             //int randomState = rng.Next(0, 1);
 
             //Write the actual play area
-            for (int w = 0; w < width; w++)
+            for (int h = 0; h < height; h++)
             {
-                for (int h = 0; h < height; h++)
+                for (int w = 0; w < width; w++)
                 {
-                    current[w, h] = coming[w, h] = deadCell;
+                    current[h, w] = coming[h, w] = deadCell;
 
-                    //Only for testing, makes a square which should not move or die
-
-                    current[5, 5] = liveCell;
-                    current[5, 6] = liveCell;
-                    current[5, 7] = liveCell;
-                    current[6, 7] = liveCell;
-                    current[6, 6] = liveCell;
-                    current[6, 5] = liveCell;
-                    current[7, 5] = liveCell;
-
+                    //current[5, 5] = liveCell;
+                    //current[5, 6] = liveCell;
+                    //current[5, 7] = liveCell;
+                    //current[4, 6] = liveCell;
+                    
+                    //current[5, 5] = liveCell;
+                    //current[5, 6] = liveCell;
+                    //current[5, 7] = liveCell;
+                    //current[6, 7] = liveCell;
+                    //current[6, 6] = liveCell;
+                    //current[6, 5] = liveCell;
+                    //current[7, 5] = liveCell;
                     
                     current[5, 5] = liveCell;
                     current[5, 6] = liveCell;
@@ -64,9 +66,9 @@ namespace Cells
                     current[5, 8] = liveCell;
                     current[5, 9] = liveCell;
                     current[5, 10] = liveCell;
-                    
-                    Console.Write(current[w, h]);
-                    coming[w, h] = current[w, h];
+
+                    Console.Write(current[h, w]);
+                    coming[h, w] = current[h, w];
                 }
                 Console.WriteLine();
             }
@@ -75,7 +77,6 @@ namespace Cells
             StartGame();
         }
 
-        static int neighbors = 0;
         static void StartGame()
         {
             int generation = 1;
@@ -87,39 +88,42 @@ namespace Cells
             {
                 //Copy current array to the next gen
                 Array.Copy(current, coming, height * width);
+                int neighbors = 0;
 
                 //Select what dot to check for neighbors
-                for (int w = 1; w < width; w++)
+                for (int h = 0; h < height; ++h)
                 {
-                    for (int h = 1; h < height; h++)
+                    for (int w = 0; w < width; ++w)
                     {
-                        //Set state of current cell
-                        char cellState = current[w, h];
+                        if (h.Equals(0) || h.Equals(height - 1) || w.Equals(0) || w.Equals(width - 1))
+                            continue;
 
-                        //Get current cells neighbors
-                        neighbors = CountNeighbors(w, h, cellState);
+                        char cellState = current[h, w];//Set state of current cell
+
+                        neighbors = CountNeighbors(h, w, cellState);//Get current cells neighbors
+
 
                         //Rules for cell reproduction and decay
                         if (cellState == deadCell && neighbors == 3) //Dead and 3 neighbors -> Lives next round
                         {
-                            coming[w, h] = liveCell;
+                            coming[h, w] = liveCell;
                         }
                         else if (cellState == liveCell && (neighbors < 2 || neighbors > 3)) //Live and less than 2 or more than 3 neighbors ---> Dead next round
                         {
-                            coming[w, h] = deadCell;
+                            coming[h, w] = deadCell;
                         }
                         else if (cellState == liveCell && (neighbors == 2 || neighbors == 3)) // Live cell has 2 or 3 neighbors survives
                         {
-                            coming[w, h] = liveCell;
+                            coming[h, w] = liveCell;
                         }
                         else
-                            coming[w, h] = cellState;
+                            coming[h, w] = cellState;
                         
 
                         neighbors = 0;
 
                         //Print the next generation after all cells has been checked
-                        Console.Write(coming[w, h]);
+                        Console.Write(coming[h, w]);
                     }
                     Console.WriteLine();
                 }
@@ -134,7 +138,7 @@ namespace Cells
                 Console.ForegroundColor = ChangeColor();
 
                 //Add some delay to be able to see anything
-                Thread.Sleep(50);
+                Thread.Sleep(100);
                 Console.Clear();
 
                 //reset for next check
@@ -142,31 +146,20 @@ namespace Cells
             }
         }
 
-        static int CountNeighbors(int w, int h, char cellState)
+        static int CountNeighbors(int h, int w, char cellState)
         {
+            int neighbors = 0;
             for (int i = -1; i <= 1; i++)
             {
                 for (int j = -1; j <= 1; j++)
                 {
-                    int widthIndex = w + i;
-                    int heightIndex = h + j;
-
-                    //If the current index is outside the array, just ignore it
-                    if (widthIndex > height - 1 || heightIndex > width - 1)
+                    if (i == 0 && j == 0)//Skip the center cell
                         continue;
 
-                    int p = (h + j);
-                    int s = (w + i);
-
-                    if (current[s, p] == liveCell)
+                    if (current[h + i, w + j] == liveCell)
                         neighbors++;
                 }
             }
-
-            //remove the cell being checked
-            if (cellState == liveCell)
-                neighbors--;
-
             return neighbors;
         }
 
